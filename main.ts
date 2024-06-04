c87b79e869285c3c56f085d270ceb3d9d5b90d49
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2024-06-04 10:57:28 +0700

    commit: turn off light

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
+const IP = "192.168.100.123";
 
+const conn = await Deno.connect({
+  hostname: IP,
+  port: 55443,
+  transport: "tcp",
+});
+
+const encoder = new TextEncoder();
+await conn.write(
+  encoder.encode('{"id":1,"method":"set_power","params":["off"]}'),
+);
